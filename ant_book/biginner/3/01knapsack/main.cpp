4f371226e0a6e201b46340835ad03d6f98cafe1c
whileD <whiled.tech@gmail.com>
2018-02-26 00:44:52 +0900

    add 01knapsack

---
+++ ant_book/biginner/3/01knapsack/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int N, W;
+vector<int> w;
+vector<int> v;
+vector<vector<int>> table;
+
+int dp(int n, int j) { // n個目,現在jの重さ
+  if (n == N) return 0; // 入れるものが無い場合
+  if (table[n][j] > 0) return table[n][j];
+  if (w[n] > j) return dp(n+1, j); // 入れられない場合
+  return max(dp(n+1, j), dp(n+1, j-w[n]) + v[n]); // 入れない場合と入れる場合で大きい方を返す
+} 
+
+int main(){
+  cin >> N >> W;
+  w = vector<int>(N);
+  v = vector<int>(N);
+  table = vector<vector<int>>(N+1, vector<int>(W+1, 0));
+  for (int i=0; i<N; i++) cin >> w[i] >> v[i];
+
+  cout << dp(0, W) << endl;
+} 
---
+++ ant_book/biginner/3/01knapsack/main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main(){
+}
