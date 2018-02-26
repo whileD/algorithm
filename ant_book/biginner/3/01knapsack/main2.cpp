0c85cce9df56854a6ec3b0ea233eb059d62ae601
whileD <whiled.tech@gmail.com>
2018-02-27 01:17:23 +0900

    漸化式バージョン追加

--- ant_book/biginner/3/01knapsack/main2.cpp
+++ ant_book/biginner/3/01knapsack/main2.cpp
@@ -7,5 +7,25 @@
 #include <stack>
 using namespace std;
 
+// 漸化式をとく実装
 int main(){
+  int N, W;
+  cin >> N >> W;
+  vector<int> w(N);
+  vector<int> v(N);
+  for (int i=0; i<N; i++) cin >> w[i] >> v[i];
+
+  vector<vector<int>> dp(N+1, vector<int>(W+1, 0));
+
+  // i番目までのものを重さの総和がk以下になるように選んだときの価値の最大値
+  for (int i=0; i<N; i++) { 
+    for (int k=0; k<=W; k++) {
+      if (k < w[i]) {
+        dp[i+1][k] = dp[i][k];
+      } else {
+        dp[i+1][k] = max(dp[i][k], dp[i][k-w[i]] + v[i]);
+      }
+    }
+  }
+  cout << dp[N][W] << endl;
 }
